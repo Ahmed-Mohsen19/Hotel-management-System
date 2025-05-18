73f414952566ff454d136a55d133955935e2d710
Ro2ya <R.Othman18221@student.aast.edu>
2025-05-18 23:58:25 +0300

    Delete nubianhotel/model directory

--- nubianhotel/model/RoomFactory.java
+++
@@ -1,8 +0,0 @@
-package com.example.nubian_hotel.model;
-
-
-public class RoomFactory {
-    public static RoomType createRoom(String name, double price, int available) {
-        return new RoomType(name, price, available);
-    }
-}
